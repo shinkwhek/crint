f4140068cc3a8889618360975c7bf6d123e31e30
shinkwhek <klklpshin@gmail.com>
2018-02-15 22:21:55 +0900

    [fix] print

--- src/main.ml
+++ src/main.ml
@@ -4,14 +4,16 @@ let limit = ref 1000
 let _ =
   Random.self_init ();
 
-  print_endline "==== ==== ==== CREATE INTEGRAL ==== ==== ====";
+  print_endline "==== ==== ==== ==== ===== ==== ==== ==== ====";
+  print_endline "====           CREATE INTEGRAL           ====";
+  print_endline "==== ==== ==== ==== ===== ==== ==== ==== ====";
 
   let e = Randomize.expr 3 |> Simplify.iter !limit in
 
-  print_endline "---- ---- ---- Type ---- ---- ----";
+  print_endline "---- ---- ---- Source Type ---- ---- ----";
   print_endline(Ast.show(e));
 
-  print_endline "---- ---- ---- LaTeX ---- ---- ----";
+  print_endline "---- ---- ---- Source ---- ---- ----";
   print_endline(Latex.eval(e));
 
   print_endline "---- ---- ---- Diff ---- ---- ----";
